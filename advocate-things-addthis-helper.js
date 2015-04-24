2ca263f2149c515eb7e6e16ba159130fcb2871d2
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-04-24 14:28:29 +0100

    Primitive version of the helper

--- advocate-things-addthis-helper.js
+++ advocate-things-addthis-helper.js
@@ -1,11 +1,12 @@
+// Handle appending token to the share url
 AT.addEventListener(AT.Events.SharepointSaved, function (meta) {
-	var urlToShare = constructUrlToShare(meta[0].token);
+	var urlToShare = AT_ConstructUrlToShare(meta[0].token);
 	addthis.update('share', 'url', urlToShare);
 	addthis.url = urlToShare;
 	addthis.ready();
 });
 
-function constructUrlToShare(token) {
+function AT_ConstructUrlToShare(token) {
 	var parser = document.createElement('a');
 	// If someone has injected the addthis url then use that, else use the default browser url
 	parser.href  = (addthis.url) ? addthis.url : window.top.location.href;
@@ -26,3 +27,22 @@ function constructUrlToShare(token) {
 		(parser.search? parser.search + '&DA=' + token : '?DA=' + token) +
 		parser.hash;
 }
+
+// On share send at data to sharepoint collector
+addthis.addEventListener('addthis.menu.share', function AT_AddThisShareEventHandler(evt) {
+	if (evt.type == 'addthis.menu.share') {
+		// Save the shareChannel in the at object
+        window.advocate_things_data._at.shareChannel = evt.data.service;
+
+        // Send sharepoint data
+        // If serviceShare is defined in client config, it will be picked up by the engine otherwise it
+        // will stick to the default, this will allow multiple sharepoint on addthis buttons.
+        //
+        // At the moment this will probably have a collision if someone has more than one website with 
+        // addthis buttons, to get around that one should create separate clients possibly or come up 
+        // with another solution This could be made even more generic by reading a specific var the at
+        // object and appending it to the name, making the sharepoint definition unique for the client's
+        // website.
+        AT.sendSharepoint(evt.data.service + 'Share', window.advocate_things_data);
+    }
+});
