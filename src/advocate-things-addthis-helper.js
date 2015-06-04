e338ae9631753eec71c08767f71c24e6418b6785
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-06-04 15:17:50 +0100

    Making error proof

--- src/advocate-things-addthis-helper.js
+++ src/advocate-things-addthis-helper.js
@@ -7,18 +7,39 @@
 
 var addthisHelper = {
 	// Handle appending token to the share url
-	registerSharepointSaveHandler: function() {
+	RegisterSharepointSaveHandler: function() {
 		if(typeof AT !== 'undefined') {
 			AT.addEventListener(AT.Events.SharepointSaved, function (meta) {
-				var queryParamName, urlToShare;
-
-				urlToShare = addthisHelper.constructUrlToShare(meta[0].token, meta[0].queryParamName);
-				console.log('urltoshare: ' + urlToShare);
-
+				var urlToShare = addthisHelper.constructUrlToShare(meta[0].token, meta[0].queryParamName);
+				
 				addthisHelper.sharePage(urlToShare);
 			});
 		} else {
 			addthisHelper.error('AT var not found');
+		}
+	},
+	// On share send at data to sharepoint collector
+	RegisterAddthisShareListener: function() {
+		if(typeof addthis !== 'undefined') {
+			addthis.addEventListener('addthis.menu.share', function AT_AddThisShareEventHandler(evt) {
+				// This is weird since this is what we were listening for anyway... taken from addthis example
+				if (evt.type == 'addthis.menu.share') {
+					// Check if the _at object is set, if not throw error in console
+					if(typeof window.advocate_things_data == 'undefined') {
+						addthisHelper.error('_at object/sharepointName not set! Aborting');
+
+						return false;
+					}
+
+					// Save the shareChannel in the at object
+			        window.advocate_things_data._at.shareChannel = evt.data.service;
+
+			        // Send sharepoint data
+			        AT.sendSharepoint(window.advocate_things_data._at.sharepointName, window.advocate_things_data);
+			    }
+			});
+		} else {
+			addthisHelper.error('Addthis var is not defined');
 		}
 	},
 	sharePage: function(urlToShare) {
@@ -51,34 +72,10 @@ var addthisHelper = {
 
 		return url;
 	},
-	// On share send at data to sharepoint collector
-	registerAddthisShareListener: function() {
-		if(typeof addthis !== 'undefined') {
-			addthis.addEventListener('addthis.menu.share', function AT_AddThisShareEventHandler(evt) {
-				if (evt.type == 'addthis.menu.share') {
-					// Save the shareChannel in the at object
-			        window.advocate_things_data._at.shareChannel = evt.data.service;
-
-			        // Send sharepoint data
-			        // If serviceShare is defined in client config, it will be picked up by the engine otherwise it
-			        // will stick to the default, this will allow multiple sharepoint on addthis buttons.
-			        //
-			        // At the moment this will probably have a collision if someone has more than one website with 
-			        // addthis buttons, to get around that one should create separate clients possibly or come up 
-			        // with another solution This could be made even more generic by reading a specific var the at
-			        // object and appending it to the name, making the sharepoint definition unique for the client's
-			        // website.
-			        AT.sendSharepoint(window.advocate_things_data._at.sharepointName, window.advocate_things_data);
-			    }
-			});
-		} else {
-			addthisHelper.error('Addthis var is not defined');
-		}
-	},
 	error: function(errorMessage) {
-		console.log('addthisHelper: ' + errorMessage);
+		console.warn('addthisHelper: ' + errorMessage);
 	}
 };
 
-addthisHelper.registerAddthisShareListener();
-addthisHelper.registerSharepointSaveHandler();
+addthisHelper.RegisterAddthisShareListener();
+addthisHelper.RegisterSharepointSaveHandler();
